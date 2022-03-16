01f2e8d9571b038ce7ebfe64f624134ee2d82fc2
eltont45 <83630909+eltont45@users.noreply.github.com>
2022-03-15 23:36:46 -0700

    post routes

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -7,6 +7,7 @@ router.get('/', (req, res) => {
     console.log('======================');
     Post.findAll({
         attributes: ['id', 'post_url', 'title', 'created_at'],
+        order: [['created_at', 'DESC']],
         include: [
             {
                 model: User,
