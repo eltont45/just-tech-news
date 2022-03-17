505e96d40eb96765487213a23e85ade36ce48303
eltont45 <83630909+eltont45@users.noreply.github.com>
2022-03-17 01:17:36 -0700

    login and logout routes and handlebar

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,6 +3,8 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
+    console.log(req.session);
+
     Post.findAll({
         attributes: [
             'id',
@@ -27,13 +29,22 @@ router.get('/', (req, res) => {
         ]
     })
         .then(dbPostData => {
-            // pass a single post object into the homepage template
-            res.render('homepage', dbPostData[0].get({ plain: true }));
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render('homepage', { posts });
         })
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
         });
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    
+    res.render('login');
 });
 
 module.exports = router;
